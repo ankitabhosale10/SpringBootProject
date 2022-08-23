2ab11b11d05f7e7fd0ce511b7d0654afadfbe703
Ankita <ankita.bhosale@gtcsys.com>
2022-08-23 17:26:07 +0530

    email html page

--- src/main/java/com/example/managementbackend/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package com.example.managementbackend.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
--- src/main/java/com/example/managementbackend/email/EmailService.java
+++ src/main/java/com/example/managementbackend/email/EmailService.java
@@ -1,41 +1,11 @@
 package com.example.managementbackend.email;
 
-import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
+public interface EmailService {
+    // Method
+    // To send a simple email
+    String sendSimpleMail(EmailDetails details);
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-@Service
-@AllArgsConstructor
-public class EmailService implements EmailSender{
-
-    private final static Logger LOGGER = LoggerFactory
-            .getLogger(EmailService.class);
-
-    private final JavaMailSender mailSender;
-
-    @Override
-    @Async
-    public void send(String to, String email) {
-
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "utf-8");
-            helper.setText(email, true);
-            helper.setTo(to);
-            helper.setSubject("Confirm your email");
-            helper.setFrom("ankitarbhosale17@gmail.com");
-
-            mailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            LOGGER.error("failed to send email", e);
-            throw new IllegalStateException("failed to send email");
-        }
-    }
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment(EmailDetails details);
 }
